8112831efc92c55d33d0c9d875ddf16f1a03da7b
Vaibhav Bhasin <vabhasin@amazon.com>
2022-07-27 19:31:05 -0700

    fix: Use ListTagsForResource response for Read Handler

--- aws-greengrassv2-componentversion/src/main/java/software/amazon/greengrassv2/componentversion/ReadHandler.java
+++ aws-greengrassv2-componentversion/src/main/java/software/amazon/greengrassv2/componentversion/ReadHandler.java
@@ -7,7 +7,6 @@ package software.amazon.greengrassv2.componentversion;
 
 import software.amazon.awssdk.services.greengrassv2.GreengrassV2Client;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
-import software.amazon.cloudformation.proxy.HandlerErrorCode;
 import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ProxyClient;
@@ -17,31 +16,49 @@ public class ReadHandler extends BaseHandlerStd {
     private Logger logger;
 
     protected ProgressEvent<ResourceModel, CallbackContext> handleRequest(
-        final AmazonWebServicesClientProxy proxy,
-        final ResourceHandlerRequest<ResourceModel> request,
-        final CallbackContext callbackContext,
-        final ProxyClient<GreengrassV2Client> proxyClient,
-        final Logger logger) {
+            final AmazonWebServicesClientProxy proxy,
+            final ResourceHandlerRequest<ResourceModel> request,
+            final CallbackContext callbackContext,
+            final ProxyClient<GreengrassV2Client> proxyClient,
+            final Logger logger) {
 
         this.logger = logger;
 
 
         return ProgressEvent.progress(request.getDesiredResourceState(), callbackContext)
                 .then(this::validateArnPresent)
-                .then(event ->
-                        proxy.initiate("AWS-GreengrassV2-ComponentVersion::Read", proxyClient, request.getDesiredResourceState(), callbackContext)
-                                .translateToServiceRequest(Translator::translateToReadRequest)
-                                .makeServiceCall((awsRequest, client) -> {
-                                    try {
-                                        return client.injectCredentialsAndInvokeV2(awsRequest, client.client()::describeComponent);
-                                    } catch (Exception ex) {
-                                        throw ExceptionTranslator.translateToCfnExceptionForCreatedResource(
-                                                "DescribeComponent", request.getDesiredResourceState().getArn(), ex);
-                                    }
-                                })
-                                .done(describeComponentResponse -> {
-                                    return ProgressEvent.success(Translator.translateFromReadResponse(describeComponentResponse), callbackContext);
-                                }));
+                .then(event -> {
+                    return proxy.initiate("AWS-GreengrassV2-ComponentVersion::Read::DescribeComponent", proxyClient, request.getDesiredResourceState(), callbackContext)
+                            .translateToServiceRequest(Translator::translateToReadRequest)
+                            .makeServiceCall((awsRequest, client) -> {
+                                try {
+                                    return client.injectCredentialsAndInvokeV2(awsRequest, client.client()::describeComponent);
+                                } catch (Exception ex) {
+                                    throw ExceptionTranslator.translateToCfnExceptionForCreatedResource(
+                                            "DescribeComponent", request.getDesiredResourceState().getArn(), ex);
+                                }
+                            }).done(describeComponentResponse -> {
+                                return ProgressEvent.progress(Translator.translateFromReadResponse(describeComponentResponse), callbackContext);
+                            });
+                })
+                // Read Request should list out the tags for the resource. DescribeComponent API doesn't include tags in the response
+                // For this we use the ListTagsForResource API to add tags to the output of DescribeComponentResponse from above.
+                // Tags information is only added if it's not empty.
+                .then(event -> {
+                    return proxy.initiate("AWS-GreengrassV2-ComponentVersion::Read::ListTagsForResource", proxyClient, request.getDesiredResourceState(), callbackContext)
+                            .translateToServiceRequest(Translator::translateToListTagsRequest)
+                            .makeServiceCall((awsRequest, client) -> {
+                                try {
+                                    return client.injectCredentialsAndInvokeV2(awsRequest, client.client()::listTagsForResource);
+                                } catch (Exception ex) {
+                                    throw ExceptionTranslator.translateToCfnExceptionForCreatedResource(
+                                            "ListTagsForResource", request.getDesiredResourceState().getArn(), ex);
+                                }
+                            })
+                            .done(listTagsForResourceResponse -> {
+                                return ProgressEvent.success(Translator.translateFromListTagsResponse(event.getResourceModel(), listTagsForResourceResponse), callbackContext);
+                            });
+                });
     }
 
 }
